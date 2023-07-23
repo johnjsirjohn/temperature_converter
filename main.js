c4ac55c275a44f275d885b562f43c9f1f2c10c4e
johnjsirjohn <50940935+johnjsirjohn@users.noreply.github.com>
2023-07-23 16:12:04 +0200

    added a celsius to kelvin converter

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 function tempConverter(valNum) {
     valNum = parseFloat(valNum)
     document.getElementById('outputFahrenheit').innerHTML=Math.floor(valNum * 9 / 5 + 32);
+    document.getElementById('outputKelvin').innerHTML=Math.floor(valNum + 273.15)
 }
