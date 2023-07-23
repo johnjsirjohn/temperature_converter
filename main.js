d328a9368bc11a3b639be6aff67a7c1b8e4f6232
johnjsirjohn <50940935+johnjsirjohn@users.noreply.github.com>
2023-07-23 16:05:28 +0200

    Update without decimals main.js

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 function tempConverter(valNum) {
     valNum = parseFloat(valNum)
-    valNum = Math.floor(valNum)
-    document.getElementById('outputFahrenheit').innerHTML=valNum * 9 / 5 + 32;
+    document.getElementById('outputFahrenheit').innerHTML=Math.floor(valNum * 9 / 5 + 32);
 }
